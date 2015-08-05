43b801d2428d5c99f5822943d783496f6ee0eb0a
Steve Loughran <stevel@hortonworks.com>
2015-08-05 15:18:59 -0700

    Steve's version of this tool. Property driven, more robust against different JSON responses coming back, some log4j logging, exit codes

--- src/main/scala/com/databricks/fetcher/JenkinsFetcher.scala
+++ src/main/scala/com/databricks/fetcher/JenkinsFetcher.scala
@@ -1,15 +1,19 @@
 package com.databricks.fetcher
 
-import java.io.{FileNotFoundException, FileWriter}
+import java.io.{IOException, FileNotFoundException, FileWriter}
 import java.net.URL
+import java.util.Date
 
+import scala.annotation.tailrec
 import scala.collection.JavaConversions._
 import scala.collection.mutable
 
+import com.databricks.util.{FailedSuite, PropertiesReader}
 import com.fasterxml.jackson.databind.{JsonNode, ObjectMapper}
 import org.apache.commons.io.IOUtils
+import org.apache.hadoop.util.ExitUtil.ExitException
 
-import com.databricks.util.{FailedSuite, PropertiesReader}
+import org.apache.spark.Logging
 
 /**
  * A class that fetches test reports from Jenkins and logs test failure
@@ -17,11 +21,16 @@ import com.databricks.util.{FailedSuite, PropertiesReader}
  * a single instance of a failed suite.
  */
 class JenkinsFetcher(
+    val urlbase: String,
     val allJenkinsProjects: Seq[String],
     val outputFileName: String,
     val outputFileDelimiter: String,
-    maxTestAgeSeconds: Long) {
+    maxTestAgeSeconds: Long,
+    val packages: String,
+    val emptyString: String = "N/A") extends Logging{
   import JenkinsFetcher._
+
+
 
   private val jsonMapper = new ObjectMapper
   private val writer = new FileWriter(outputFileName)
@@ -30,7 +39,7 @@ class JenkinsFetcher(
     .filter(_ >= 0)
     .getOrElse(Long.MaxValue)
 
-  log(s"Fetching Jenkins test reports from ${JENKINS_URL_BASE}/[...]")
+  log(s"Fetching Jenkins test reports for packages under $packages from ${urlbase}/[...]")
   log(s"Logging test failure information to ${outputFileName}\n")
 
   // Write the header
@@ -41,11 +50,16 @@ class JenkinsFetcher(
    * Fetch test reports for each of the configured Jenkins projects
    * and log information about any test failures to the output file.
    */
-  def run(): Unit = {
+  def run(): (Int, Int) = {
+    var successCount = 0
+    var attemptCount = 0
     allJenkinsProjects.foreach { projectName =>
-      val projectUrl = s"${JENKINS_URL_BASE}/$projectName"
-      handleProject(projectUrl, projectName)
+      val projectUrl = s"${urlbase}/$projectName"
+      val (attempts, successes) = handleProject(projectUrl, projectName)
+      attemptCount += attempts
+      successCount += successes
     }
+    (attemptCount, successCount)
   }
 
   /** Stop the fetcher and close any resources used in the process. */
@@ -56,61 +70,120 @@ class JenkinsFetcher(
   // e.g. https://amplab.cs.berkeley.edu/jenkins/job/SparkPullRequestBuilder
   // e.g. https://amplab.cs.berkeley.edu/jenkins/job/Spark-Master-SBT
   // e.g. https://amplab.cs.berkeley.edu/jenkins/job/Spark-Master-Maven-pre-YARN/
-  private def handleProject(projectUrl: String, projectName: String): Unit = {
+  private def handleProject(projectUrl: String, projectName: String): (Int, Int) = {
     log(s"========== Fetching test reports for project $projectName ==========")
-    val projectJson = fetchJson(projectUrl).getOrElse { return }
+    val projectJson = fetchJson(projectUrl).getOrElse { return (0, 0) }
     val firstBuild = projectJson.get("firstBuild").get("number").asInt
     val lastBuild = projectJson.get("lastBuild").get("number").asInt
     assert(lastBuild >= firstBuild)
+    log(s"number of test runs: ${lastBuild - firstBuild}")
+    var successCount = 0
+    var attemptCount = 0
     (firstBuild to lastBuild).reverse.foreach { buildNumber =>
       val buildUrl = s"${projectUrl}/$buildNumber"
-      handleBuild(buildUrl, projectName)
+      val (attempts, successes) = handleBuild(buildUrl, projectName)
+      attemptCount += attempts
+      successCount += successes
     }
+    (attemptCount, successCount)
   }
 
   // e.g. .../SparkPullRequestBuilder/28354
   // e.g. .../Spark-Master-SBT/1847
   // e.g. .../Spark-Master-Maven-pre-YARN/1760
-  private def handleBuild(buildUrl: String, projectName: String): Unit = {
-    val buildJson = fetchJson(buildUrl).getOrElse { return }
+  private def handleBuild(buildUrl: String, projectName: String): (Int, Int) = {
+    val buildJson = fetchJson(buildUrl).getOrElse { return  (0, 0) }
     val buildTimestamp = buildJson.get("timestamp").asLong
     val buildTooOld = System.currentTimeMillis - buildTimestamp > maxTestAgeMillis
+    var successCount = 0
+    var attemptCount = 0
     if (!buildTooOld) {
       // Each build in the pull request builder only has one run, so use the build URL directly
       if (isPullRequestBuilder(projectName)) {
-        handleRun(buildUrl, projectName)
+        logDebug("Pull request Builder")
+        attemptCount += 1
+        successCount += (if (handleRun(buildUrl, projectName)) 1 else 0)
       } else {
         val runsJson = buildJson.get("runs")
-        runsJson.iterator.foreach { runJson =>
-          val runUrl = runJson.get("url").asText.stripSuffix("/")
-          handleRun(runUrl, projectName)
+        if (runsJson.size()> 0) {
+
+          runsJson.iterator.foreach { runJson =>
+            attemptCount += 1
+            val runUrl = runJson.get("url").asText.stripSuffix("/")
+            successCount += (if (handleRun(buildUrl, projectName)) 1 else 0)
+          }
+        } else {
+          logInfo(s"Project $projectName has no runs")
         }
       }
+    } else {
+      logInfo(s"Build ${shorten(buildUrl)} is too old: ${new Date(buildTimestamp) }")
+    }
+    (attemptCount, successCount)
+  }
+
+  private def getJsonField(url: String, document: JsonNode, field:String): JsonNode = {
+    document.get(field) match {
+      case null =>
+        val text = s"No field '$field' in JSON at $url"
+        logDebug(text + "\n" + document.toString)
+        throw new RuntimeException(text)
+      case node => node
     }
   }
+
+  @tailrec
+  private def getJsonFields(url: String, document: JsonNode, fields: List[String]): JsonNode = {
+    fields match {
+      case Nil =>
+        document
+      case (field :: tail) =>
+        val doc2 = getJsonField (url, document, field)
+        getJsonFields(url +":" + field, doc2, tail)
+    }
+   }
 
   // e.g. .../SparkPullRequestBuilder/28354
   // e.g. .../Spark-Master-SBT/1847/AMPLAB_JENKINS_BUILD_PROFILE=hadoop1.0,label=centos/
   // e.g. .../Spark-Master-Maven-pre-YARN/1760/hadoop.version=1.0.4,label=centos/
-  private def handleRun(runUrl: String, projectName: String): Unit = wrapForIndentation {
-    log(s"Handle run ${shorten(runUrl)}")
-    val runJson = fetchJson(runUrl).getOrElse { return }
-    val runTimestamp = runJson.get("timestamp").asLong
+  private def handleRun(runUrl: String, projectName: String): Boolean = wrapForIndentation {
+    log(s"Handle run ${runUrl}")
+    val jsonURL = toJsonURL(runUrl)
+    val runJson = fetchJson(runUrl).getOrElse {
+      logDebug(s"No run report at $runUrl")
+      return false
+    }
+    val runTimestamp = getJsonField(jsonURL, runJson, "timestamp").asLong
     val testReportUrl = s"$runUrl/testReport"
-    val testReportJson = fetchJson(testReportUrl).getOrElse { return }
-    val numTestsFailed = testReportJson.get("failCount").asInt
+    val testReportJsonURL = toJsonURL(testReportUrl)
+    val testReportJson = fetchJson(testReportUrl).getOrElse {
+      logDebug(s"No test report at $testReportUrl ")
+      return false
+    }
+    val numTestsFailed = getJsonField(testReportJsonURL, testReportJson, "failCount").asInt
     if (numTestsFailed > 0) {
       wrapForIndentation {
         log(s"Found $numTestsFailed failed test(s)")
         // Suite name -> number of failed occurrences in this run
         val failedSuiteCounts = new mutable.HashMap[String, Int]
         val failedSuiteStatuses = Set("FAILED", "REGRESSION")
-        val suitesJson = testReportJson.get("suites")
+        val childReports = getJsonFields(testReportJsonURL, testReportJson,
+            List("childReports"))
+        // >1 child can come back. For now, pick #1
+        if (childReports.size() == 0) {
+          logDebug("No child reports")
+          return false;
+        }
+        val suitesJson = getJsonFields(testReportJsonURL + ":childReports",
+            childReports.get(0),
+            List("result", "suites"))
+
         suitesJson.iterator.foreach { suiteJson =>
-          val failedSuiteNames = suiteJson.get("cases").iterator
-            .filter { caseJson => failedSuiteStatuses.contains(caseJson.get("status").asText) }
-            .map { _.get("className").asText }
-            .filter { _.startsWith("org.apache.spark") }
+          val failedSuiteNames = getJsonField(testReportJsonURL, suiteJson, "cases").iterator
+            .filter { caseJson =>
+              failedSuiteStatuses.contains(getJsonField(testReportJsonURL, caseJson, "status").asText) }
+            .map { getJsonField(testReportJsonURL, _, "className").asText }
+            .filter { _.startsWith(packages) }
           failedSuiteNames.foreach { suiteName =>
             if (!failedSuiteCounts.contains(suiteName)) {
               failedSuiteCounts(suiteName) = 0
@@ -122,10 +195,13 @@ class JenkinsFetcher(
           handleSuite(suiteName, numTestsFailed, runUrl, runTimestamp, projectName)
         }
         if (failedSuiteCounts.isEmpty) {
-          log("Warning: No failed suites beginning with org.apache.spark.* found.")
+          log(s"Warning: No failed suites beginning with '$packages' found.")
         }
       }
+    } else {
+      logInfo("No test failures")
     }
+    true
   }
 
   /** Log one line that describes a particular failed suite instance to the output file. */
@@ -143,7 +219,7 @@ class JenkinsFetcher(
 
     val hadoopProfile =
       if (isPullRequestBuilder(projectName)) {
-        PULL_REQUEST_BUILDER_HADOOP_PROFILE
+        pull_request_builder_hadoop_profile
       } else {
         parseHadoopProfile(runUrl)
       }
@@ -164,13 +240,17 @@ class JenkinsFetcher(
   /** Fetch the content of the specified URL and parse it as a JsonNode. */
   private def fetchJson(url: String): Option[JsonNode] = wrapForIndentation {
     try {
-      val jsonUrl = url.stripSuffix("/") + "/" + JSON_URL_SUFFIX
-      val jsonString = IOUtils.toString(new URL(jsonUrl).openStream)
+      val jsonUrl = toJsonURL(url)
+      val src = new URL(jsonUrl)
+      log(s"GET $src")
+      val jsonString = IOUtils.toString(src.openStream)
+      // read operation closes stream afterwards
       return Some(jsonMapper.readValue(jsonString, classOf[JsonNode]))
     } catch {
       case fnf: FileNotFoundException =>
-        log(s"Unable to find JSON at ${shorten(url)}")
-        log("It is likely that this build failed style tests or did not compile.")
+        log(s"Unable to find JSON at $url")
+        log("It is likely that this build failed style tests, did not compile" +
+            " -or is still executing.")
       case e: Exception =>
         log(s"Exception when fetching JSON from ${shorten(url)}")
         wrapForIndentation { log(e.toString) }
@@ -178,9 +258,13 @@ class JenkinsFetcher(
     None
   }
 
+  def toJsonURL(url: String): String = {
+    url.stripSuffix("/") + "/" + JSON_URL_SUFFIX
+  }
+
   /** Return a shorter, relative URL that does not include the Jenkins base URL. */
   private def shorten(url: String): String = {
-    url.stripPrefix(JENKINS_URL_BASE)
+    url.stripPrefix(urlbase)
   }
 
   /** Parse the Hadoop profile from the run URL. */
@@ -188,10 +272,10 @@ class JenkinsFetcher(
     val splitKeys = Seq("AMPLAB_JENKINS_BUILD_PROFILE", "hadoop.version", "HADOOP_PROFILE")
     splitKeys.foreach { key =>
       if (url.contains(key)) {
-        return url.split(s"$key=")(1).split(",")(0)
+        url.split(s"$key=")(1).split(",")(0)
       }
     }
-    return "N/A"
+    emptyString
   }
 
   /** Translate the given hadoop profile to an expected Hadoop version. */
@@ -230,28 +314,90 @@ class JenkinsFetcher(
   
 }
 
-private object JenkinsFetcher {
-  private val JENKINS_URL_BASE = "https://amplab.cs.berkeley.edu/jenkins/job"
+private object JenkinsFetcher extends Logging {
   private val JSON_URL_SUFFIX = "api/json"
-  private val PULL_REQUEST_BUILDER_HADOOP_PROFILE = "2.3.0"
+  private val pull_request_builder_hadoop_profile = "2.3.0"
 
-  def main(args: Array[String]): Unit = {
+  val JENKINS_URL_BASE = "spark.test.jenkins.url.base"
+
+  val USAGE =
+    s"""
+      | Jenkins Fetcher
+      | ---------------
+      | '$JENKINS_URL_BASE' base URL, e.g https://amplab.cs.berkeley.edu/jenkins/job
+      | 'spark.test.jenkinsProjects' list of projects, e.g. SparkPullRequestBuilder,Spark-Master-SBT
+      | 'spark.test.outputFileName' path for output file, e.g. jenkins.csv
+      | 'spark.test.outputFileDelimiter, delimiter between entries, e.g ', '
+      | 'spark.test.packages' packages to scan, e.g. 'org.apache.spark'
+      | 'spark.test.fetcher.maxTestAge.seconds' maximum age of test runs, e.g. '600'
+    """.stripMargin
+  
+  def requiredProperty(key: String): String = {
+    sys.props(key) match {
+      case null => throw new ExitException(1, s"Unset property '$key'\n$USAGE")
+      case "" => throw new ExitException(1, s"Empty property '$key'\n$USAGE")
+      case prop => prop
+    }
+  }
+
+  /**
+   * optional property, reverting to `defVal` if the property is not defined.
+   * @param key key to look for
+   * @param defVal default value
+   * @return a string
+   */
+  def optionalProperty(key: String, defVal: String): String = {
+    sys.props(key) match {
+      case null => defVal
+      case prop => prop
+    }
+  }
+  
+  def innerMain(args: Array[String]): Int = {
     println("\n=== *~ Welcome to Spark test failure fetcher v1.0! ~* ===\n")
     PropertiesReader.loadSystemProperties()
-    val allJenkinsProjects = sys.props("spark.test.jenkinsProjects")
+    val allJenkinsProjects = requiredProperty("spark.test.jenkinsProjects")
       .split(",")
       .map(_.trim)
       .filter(_.nonEmpty)
-    val outputFileName = sys.props("spark.test.outputFileName")
-    val outputFileDelimiter = sys.props("spark.test.outputFileDelimiter")
-    val maxTestAgeSeconds = sys.props("spark.test.fetcher.maxTestAge.seconds").toLong
+
+    val jenkins_url_base = requiredProperty(JENKINS_URL_BASE)
+    val outputFileName = requiredProperty("spark.test.outputFileName").trim
+    val outputFileDelimiter = requiredProperty("spark.test.outputFileDelimiter")
+    val packages = requiredProperty("spark.test.packages").trim
+    val beforeEntry = optionalProperty("spark.test.delimiter.before","\"")
+    val afterEntry = optionalProperty("spark.test.delimiter.after","\"")
+    val emptyEntry = optionalProperty("spark.test.empty.column","")
+
+    val maxTestAgeSeconds = requiredProperty("spark.test.fetcher.maxTestAge.seconds").toLong
     val fetcher = new JenkinsFetcher(
+      jenkins_url_base,
       allJenkinsProjects,
       outputFileName,
       outputFileDelimiter,
-      maxTestAgeSeconds)
-    fetcher.run()
+      maxTestAgeSeconds,
+      packages,
+      emptyEntry)
+    val (attempts, successes) = fetcher.run()
     fetcher.stop()
+    println(s"$attempts attempts; $successes successes")
+    if (successes > 0) 0 else 1
+  }
+
+  
+  def main(args: Array[String]): Unit = {
+    try {
+      val exitCode = innerMain(args)
+      sys.exit(exitCode)
+    } catch {
+      case ee: ExitException =>
+        println(ee.toString)
+        sys.exit(ee.status)
+      case err: Throwable =>
+        println(err)
+        err.printStackTrace(System.err)
+        sys.exit(-1)
+    }
   }
 }
 
